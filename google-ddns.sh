27c2c5909248e88bdb17e8c32703bcff42c0e4b5
sevsec <17163866+sevsec@users.noreply.github.com>
2021-11-19 16:51:40 -0800

    Create google-ddns.sh

---
+++ google-ddns.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Notes:
+# You can get the creds for this by going to your google domain, creatin a DDNS entry, and grabbing
+# the creds there.
+
+USER="USERNAME"
+PASS="PASSWORD"
+HOSTNAME="HOST.EXAMPLE.COM"
+URL="https://$USER:$PASS@domains.google.com/nic/update?hostname=$HOSTNAME&myip="
+IP=$(curl -s https://domains.google.com/checkip)
+echo `date` `curl -s -X POST $URL$IP`
