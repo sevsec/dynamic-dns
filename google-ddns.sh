3540a7f4963452ceea169149db25e3918cdf7abb
sevsec <17163866+sevsec@users.noreply.github.com>
2021-11-19 16:58:05 -0800

    Update google-ddns.sh
    Script comment update

--- google-ddns.sh
+++ google-ddns.sh
@@ -8,5 +8,8 @@ USER="USERNAME"
 PASS="PASSWORD"
 HOSTNAME="HOST.EXAMPLE.COM"
 URL="https://$USER:$PASS@domains.google.com/nic/update?hostname=$HOSTNAME&myip="
+
 IP=$(curl -s https://domains.google.com/checkip)
+
+# This last line I echo to a log file using crontab, completely optional.
 echo `date` `curl -s -X POST $URL$IP`
